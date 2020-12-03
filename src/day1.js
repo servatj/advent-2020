311e03347da59ff7593f53068a55ff28914b6989
servatj <jservatlorca@gmail.com>
2020-12-03 18:21:25 +0100

    day1 solution advent

---
+++ src/day1.js
@@ -0,0 +1,27 @@
+const appRoot = require('app-root-path');
+const fs = require("fs");
+const readline = require("readline");
+
+let years = []
+
+const streamFile = fs.createReadStream(
+  appRoot + '/data/day1.txt'
+);
+
+const lineReader = readline.createInterface({
+  input: streamFile,
+});
+
+lineReader.on("line", async (line) => {
+  years = [...years, line]
+})
+
+lineReader.on("close", async (line) => {
+  years.forEach((value, index) => {
+    for(let i = index ; i <= years.length; i ++) {
+       if(Number(value) + Number(years[i]) === 2020) {
+         console.log(value * years[i])
+       }
+    }
+  })
+})
