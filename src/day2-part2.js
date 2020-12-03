88bd87a31a36f8f37c75fd137a1557b42615ba05
servatj <jservatlorca@gmail.com>
2020-12-03 02:01:17 +0100

    second part

---
+++ src/day2-part2.js
@@ -0,0 +1,57 @@
+const appRoot = require('app-root-path');
+const fs = require("fs");
+const readline = require("readline");
+
+let valids = 0;
+
+const countTimes = (character, password) => {
+  return password.split('').filter( x => x === character ).length
+}
+
+const extractInterval = (line) => {
+  return line.split(' ')[0];
+}
+
+const extractLetter = (line) => {
+  return line.split(' ')[1].replace(':', '');
+}
+
+const extractPassword = (line) => {
+  return line.split(' ')[2];
+}
+
+const validPosition = (letter, position, password) => {
+  return password[Number(position) - 1] === letter
+} 
+
+const streamFile = fs.createReadStream(
+  appRoot + '/data/passwords'
+);
+
+const lineReader = readline.createInterface({
+  input: streamFile,
+});
+
+lineReader.on("line", async (line) => {
+
+  try {
+    const interval = extractInterval(line);
+    const letter = extractLetter(line);
+    const password = extractPassword(line);
+  
+    const times = countTimes(letter, password);
+  
+    const low = interval.split('-')[0];
+    const high = interval.split('-')[1];
+    console.log(line, interval, letter, password)
+    if((validPosition(letter, low, password) && !validPosition(letter, high, password)) || (!validPosition(letter, low, password) && validPosition(letter, high, password))) {
+      valids++
+    }
+    
+  } catch (error) {
+     console.log(error)
+  }
+
+
+  console.log(valids);
+})
