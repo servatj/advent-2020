d27fb9abb5f729efce1c5f620d5bb58e2610bf8a
servatj <jservatlorca@gmail.com>
2020-12-03 01:29:17 +0100

    first solution

--- src/day2.js
+++ src/day2.js
@@ -2,6 +2,23 @@ const appRoot = require('app-root-path');
 const fs = require("fs");
 const readline = require("readline");
 
+let valids = 0;
+
+const countTimes = (character, password) => {
+  return password.split('').filter( x => x === character ).length
+}
+
+const extractInterval = (line) => {
+  return line.split(' ')[0]
+}
+
+const extractLetter = (line) => {
+  return line.split(' ')[1].replace(':', '');
+}
+
+const extractPassword = (line) => {
+  return line.split(' ')[2]
+}
 
 const streamFile = fs.createReadStream(
   appRoot + '/data/passwords'
@@ -11,8 +28,26 @@ const lineReader = readline.createInterface({
   input: streamFile,
 });
 
-lineReader.on("line", async (data) => {
-  
-  console.log(data)
+lineReader.on("line", async (line) => {
 
+  try {
+    const interval = extractInterval(line);
+    const letter = extractLetter(line);
+    const password = extractPassword(line);
+  
+    const times = countTimes(letter, password);
+  
+    const low = interval.split('-')[0];
+    const high = interval.split('-')[1];
+    console.log(line, interval, letter, password)
+    if(times >= low && times <= high) {
+      valids++
+    }
+    
+  } catch (error) {
+     console.log(error)
+  }
+
+
+  console.log(valids);
 })
