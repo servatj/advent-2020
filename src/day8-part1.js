0970817b1f87d42ba2241e171a2076af6582db4a
servatj <jservatlorca@gmail.com>
2020-12-13 11:29:19 +0100

    Add 8 part 1

---
+++ src/day8-part1.js
@@ -0,0 +1,51 @@
+const appRoot = require('app-root-path');
+const fs = require("fs");
+
+const filename = appRoot + '/data/assemblyData.txt';
+const instructions = fs.readFileSync(filename, 'utf-8');
+
+
+const instArray = instructions.split('\n');
+
+let keepRunning = true;
+
+let acc = 0;
+let processedInst = [];
+let pointer = 0;
+let currentInstruction = '';
+
+const isSecondTime = (processed, value) => {
+  return processed.filter(x => x === value).length > 1;
+}
+
+const instructionOps = (command, value) => {
+  const ops =  {
+    acc: () => {
+      acc = acc + Number(value.replace('+', ''));
+      pointer = pointer + 1;
+    },
+    jmp: () => {
+      pointer = pointer  + (Number(value.replace('+', '')) );
+
+    },
+    nop: () => {
+      pointer = pointer +1;
+    },
+    default: () => console.log('instruction not found')
+  }
+  //ops[command]() 
+  ops[command] ? ops[command]() : ops['default']();
+}
+
+while(keepRunning){
+  currentInstruction = instArray[pointer];
+  const [command, value] = currentInstruction.split(' ');
+  if (!isSecondTime(processedInst, pointer)) {
+    instructionOps(command, value);
+    processedInst = [...processedInst, pointer];
+  } else {
+    keepRunning = false;
+  }
+}
+
+console.log('acc', acc)
